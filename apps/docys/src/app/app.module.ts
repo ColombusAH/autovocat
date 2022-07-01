c995b296cf0230c3e1a573b2b127f3e4eab3cf16
Avner <avner.h@nanox.vision>
2022-07-01 13:31:47 +0300

    refac

--- apps/docys/src/app/app.module.ts
+++ apps/docys/src/app/app.module.ts
@@ -5,12 +5,7 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { Route, RouterModule } from '@angular/router';
 import { CoreModule } from './core/core.module';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { environment } from '../environments/environment';
+
 
 const routes: Route[] = [
   {
@@ -32,11 +27,7 @@ const routes: Route[] = [
     BrowserModule,
     RouterModule.forRoot(routes),
     CoreModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFirestoreModule,
-    AngularFireStorageModule,
-    AngularFireDatabaseModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/docys/src/app/core/core.module.ts
+++ apps/docys/src/app/core/core.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+  ],
+  exports: [
+    AngularFireModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
